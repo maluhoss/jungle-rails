736a2ee9567630c95e75da7d21d3437115ffa310
Maliha Hossain <malu.hoss@gmail.com>
2019-04-22 18:34:23 +0000

    refactored rspec tests to use errors full messages to validate attributes

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validation' do
-    category = Category.new(name: "Books")
+    category = Category.create!(name: "Books")
 
     it "is valid with valid attributes" do
       product = category.products.new ({
@@ -10,43 +10,48 @@ RSpec.describe Product, type: :model do
         price: 56,
         quantity: 10
       })
-      expect(product).to be_valid
+      # expect(product).to be_valid
+      expect(product.errors.full_messages).to eq([])
     end
 
     it "is not valid without a name" do
-      product = category.products.new({
+      product = category.products.create({
         name: nil,
         price: 56,
         quantity: 2
       })
-      expect(product).to_not be_valid
+      # expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Name can't be blank")
     end
 
     it "is not valid without a price" do
-      product = category.products.new({
+      product = category.products.create({
         name: "Maliha's cards",
         price: nil,
         quantity: 2
       })
-      expect(product).to_not be_valid
+      # expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Price can't be blank")
     end
 
     it "is not valid without a quantity" do
-      product = category.products.new({
+      product = category.products.create({
         name: "Maliha's cards",
         price: 56,
         quantity: nil
       })
-      expect(product).to_not be_valid
+      # expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Quantity can't be blank")
     end
 
     it "is not valid without a category" do
-      product = Product.new({
+      product = Product.create({
         name: "Maliha's cards",
         price: 56,
         quantity: 10
       })
-      expect(product).to_not be_valid
+      # expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Category can't be blank")
     end
 
   end

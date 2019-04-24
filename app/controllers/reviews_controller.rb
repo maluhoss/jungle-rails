42528d60e8ff807db62ea7640c81ee77e45c0bd9
Maliha Hossain <malu.hoss@gmail.com>
2019-04-24 04:32:28 +0000

    reviews displayed in reverse chronological order and only logged in users allowed to create reviews or redirected to login page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,6 @@
 class ReviewsController < ApplicationController
+  before_filter :authorize
+
   def create
     @product = Product.find(params[:product_id])
     @review = @product.reviews.new(reviews_params)
@@ -10,6 +12,13 @@ class ReviewsController < ApplicationController
       render 'products/show'
     end
 
+  end
+
+  def destroy
+    @product = Product.find params[:id]
+    @review = @product.reviews.find(reviews_params)
+    @review.destroy
+    redirect_to product_path(@product), notice: 'Review deleted!'
   end
 
   private
